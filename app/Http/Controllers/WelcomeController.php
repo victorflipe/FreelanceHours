bfd93dd08797b9d7183fd8ff023e8d462d05e4aa
victorroli <victorfelipe1608@gmail.com>
2024-10-08 00:00:27 -0300

    wip livewire

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return view('welcome')
+        // $user = User::find(1);
+        $user = User::query()->create([
+            'name' => 'Victor',
+            'email' => 'victorf@gmail.com',
+            'password' => 'victor'
+        ]);
+
+
+        var_dump($user);die();
+        return view('welcome');
     }
 }
