c78f394fac9de436de57e66ca0828294da4f72f3
victorroli <victorfelipe1608@gmail.com>
2024-10-10 22:41:04 -0300

    create user and project models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Project;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +16,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()->count(200)->create();
+        // Project::factory()->count(10)->create();
+
+        User::query()->inRandomOrder()->limit(10)->get()
+        ->each(fn (User $u) => Project::factory()->create(['created_by' => $u->id]));
     }
 }
