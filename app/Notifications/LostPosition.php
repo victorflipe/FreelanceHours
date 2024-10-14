bac87ce5c7442d4342df0c881627f4d1e2c6eb9d
victorroli <victorfelipe1608@gmail.com>
2024-10-14 00:37:39 -0300

    Notifications for the lost positions

--- app/Notifications/LostPosition.php
+++ app/Notifications/LostPosition.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class LostPosition extends Notification
+class LostPosition extends Notification implements ShouldQueue
 {
     use Queueable;
 
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Container\Attributes\Log;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -10,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withSchedule(function(Schedule $schedule){
+        $schedule->command('app:close-project')->everyMinute();
+    })
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
